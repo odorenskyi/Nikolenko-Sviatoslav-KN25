fba0dc2ea40eb5e1f0847ed746e8ede0a043dd16
Silhouette18 <161963725+Silhouette18@users.noreply.github.com>
2026-03-21 14:00:31 +0200

    Added header file

---
+++ lab09/prj/ModulesNikolenko.h
@@ -0,0 +1,15 @@
+#ifndef MODULES_NIKOLENKO_H
+#define MODULES_NIKOLENKO_H
+
+
+double s_calculation(double x, double y, double z);
+
+
+void task_9_1(int grade);
+
+void task_9_2(int n);
+
+int task_9_3(unsigned int N);
+
+
+#endif
